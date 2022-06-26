21437ae4c6047f428f44d0b393fd420590c536ec
Mohammed Osman <mohammed.osman@smartercode.io>
2022-06-26 17:02:20 +0200

    Add project files.

---
+++ MyrspovenAssignment/Controllers/Building.cs
@@ -0,0 +1,13 @@
+﻿namespace MyrspovenAssignment.Controllers
+{
+    public class Building
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Long { get; set; }
+        public string Lat { get; set; }
+        public string DataSetStartDate { get; set; }
+        public int squaredMeters { get; set; }
+
+    }
+}
---
+++ MyrspovenAssignment/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Flurl;
+using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyrspovenAssignment.Models;
+using System.Diagnostics;
+namespace MyrspovenAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await "https://i4test.azurewebsites.net/connect/token"
+               .PostUrlEncodedAsync(
+                new
+                {
+                    client_id = "recruit",
+                    client_secret = "ey9nf7E7E6ErAkdD7nWG",
+                    grant_type = "client_credentials"
+                }).ReceiveJson<Token>();
+
+            var response = await "https://datastorage-fake-myrspoven.azurewebsites.net"
+                  .AppendPathSegment("Building/GetBuildings")
+                  .WithOAuthBearerToken(result.access_token)
+                  .PostAsync();
+            var buildings = await response.GetJsonAsync<List<Building>>().ConfigureAwait(true);
+
+
+
+
+
+            return Ok(buildings);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
